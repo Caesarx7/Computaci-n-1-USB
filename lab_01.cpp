b7fa762ba3504908d1dc4b3be16a422d873f39e5
Caesarx7 <caesarcard7@gmail.com>
2025-02-07 21:48:33 -0400

    Update lab_01.cpp

--- lab_01.cpp
+++ lab_01.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
-#include <stdlib.h>
+#include <stdlib.h>  //Libreria que permitira crear un delay antes de que se cierre el programa para mostrar los resultados
 
 int main() {
 
-    float num1, num2, num3, sum;
+    float num1, num2, num3, sum;    //Declaración de variables tipo float
 
-    std::cout << "Ingrese el primer numero: ";
+
+    std::cout << "Ingrese el primer numero: ";     //Solicitud de datos
     std::cin >> num1;
 
     std::cout << "Ingrese el segundo numero: ";
@@ -14,11 +15,12 @@ int main() {
     std::cout << "Ingrese el tercer numero: ";
     std::cin >> num3;
 
-    sum = num1 + num2 + num3;
+    sum = num1 + num2 + num3;        //Operación suma
 
-    std::cout << "La suma de los tres numeros es: " << sum << std::endl;
 
-    system("pause");
+    std::cout << "La suma de los tres numeros es: " << sum << std::endl;     //Output con el resultado
+
+    system("pause");     //Pausa como control de proceso antes de que se cierre el programa por si solo y así poder ver los resultados
 
         return 0;
 }
